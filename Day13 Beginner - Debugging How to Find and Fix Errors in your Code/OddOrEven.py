c40749d82748419b90025aad51591912db91d23d
cosmos <markadreianramos41@gmail.com>
2023-06-30 14:06:19 +0800

    adding day 12 and 13

---
+++ Day13 Beginner - Debugging How to Find and Fix Errors in your Code/OddOrEven.py
@@ -0,0 +1,17 @@
+# # Bugged Code
+# number = int(input("Which number do you want to check?"))
+
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+
+# Solution
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
